96966c45eca30cafed3f865298b88d38d3c273ad
Katrina Ni <kani@tableau.com>
2021-05-11 22:50:23 -0700

    defining extra document properties

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -7,8 +7,14 @@ interface UserAttrs {
 }
 
 // An interface that describes the properties that a User Model has
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -26,6 +32,8 @@ userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-const User = mongoose.model<any, UserModel>("User", userSchema);
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+// const user = User.build({ email: "test@test.com", password: "password" });
 
 export { User };
