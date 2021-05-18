fdf7e3947f9fe4f502d772b9c8cf57787e25321b
Katrina Ni <kani@tableau.com>
2021-05-18 00:07:52 -0700

    9. sign in logic

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
 
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
+import { User } from "../models/user";
 import { body } from "express-validator";
+import jwt from "jsonwebtoken";
 import { validateRequest } from "../middleware/validate-request";
 
 const router = express.Router();
@@ -12,7 +16,37 @@ router.post(
     body("password").trim().notEmpty().withMessage("Password must be supplied"),
   ],
   validateRequest,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordsMatch) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  }
 );
 
 export { router as signinRouter };
