6b9ad6a21abb7b52596f37c5e644110fc42b06bf
Katrina Ni <kani@tableau.com>
2021-05-20 22:05:13 -0700

    10. the first test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,31 +1,5 @@
-import "express-async-errors";
-
-import cookieSession from "cookie-session";
-import { currentUserRouter } from "./routes/current-user";
-import { errorHandler } from "./middleware/error-handler";
-import express from "express";
-import { json } from "body-parser";
+import { app } from "./app";
 import mongoose from "mongoose";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-
-const app = express();
-app.set("trust proxy", true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true, // https connection
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.use(errorHandler);
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,9 @@
+import { app } from "../../app";
+import request from "supertest";
+
+it("returns a 201 on successful signup", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(201);
+});
