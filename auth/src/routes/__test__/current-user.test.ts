fb5e02f6b1071642038df5da43250ed42a9a9e0a
Katrina Ni <kani@tableau.com>
2021-05-20 22:47:33 -0700

    10. auth helper function

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,12 +2,8 @@ import { app } from "../../app";
 import request from "supertest";
 
 it("responds with details about the current user", async () => {
-  const authResponse = await request(app)
-    .post("/api/users/signup")
-    .send({ email: "test@test.com", password: "password" })
-    .expect(201);
+  const cookie = await global.signin();
 
-  const cookie = authResponse.get("Set-Cookie");
   const response = await request(app)
     .get("/api/users/currentuser")
     .set("Cookie", cookie)
@@ -15,4 +11,13 @@ it("responds with details about the current user", async () => {
     .expect(200);
 
   expect(response.body.currentUser.email).toEqual("test@test.com");
+});
+
+it("responds with null if not authenticated", async () => {
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser).toEqual(null);
 });
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,5 +1,15 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
+import { app } from "../app";
 import mongoose from "mongoose";
+import request from "supertest";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 beforeAll(async () => {
@@ -26,3 +36,17 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close;
 });
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
