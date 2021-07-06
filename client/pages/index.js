2c2dea72e146123abc02f082dbbe046405edacb7
Katrina <lichenkatrinani@gmail.com>
2021-07-05 23:29:50 -0700

    11. specifying the host

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,29 @@
-export default () => {
+import axios from "axios";
+import axio from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+    // server
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticketing.dev",
+        },
+      }
+    );
+    return data;
+  } else {
+    // browser
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+};
+
+export default LandingPage;
