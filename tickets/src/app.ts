0f108c0dc9afd4dc5ad3cc5213f8cb3405cf3050
Katrina <lichenkatrinani@gmail.com>
2021-07-08 23:02:12 -0700

    13. routes to show tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,8 @@ import { errorHandler, currentUser } from "@kanitickets/common";
 import express from "express";
 import { json } from "body-parser";
 import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
 
 const app = express();
 app.set("trust proxy", true);
@@ -17,6 +19,9 @@ app.use(
 );
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+
 
 app.use(errorHandler);
 
