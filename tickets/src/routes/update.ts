cf22ece98de7acc16ca5dbe6635646e859ce6111
Katrina <lichenkatrinani@gmail.com>
2021-07-11 16:59:29 -0700

    15. graceful shutdown

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -6,7 +6,9 @@ import {
 } from "@kanitickets/common";
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import { TicketUpdatedPublisher } from "../events/publishers/ticket-updated-publisher";
 import { Ticket } from "../models/tickets";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 router.put(
@@ -33,6 +35,12 @@ router.put(
       price: req.body.price,
     });
     await ticket.save();
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
   }
