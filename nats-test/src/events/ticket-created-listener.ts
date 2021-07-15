2dd55ab3334c641c6a7767313ab1f70ba2a0439c
Katrina <lichenkatrinani@gmail.com>
2021-07-14 22:32:12 -0700

    16. providing a mock implementation

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,5 +1,5 @@
 import { Message } from "node-nats-streaming";
-import { Listener } from "./base-listener";
+import { Listener } from "../../../common/src/events/base-listener";
 import { Subjects } from "./subjects";
 import { TicketCreatedEvent } from "./ticket-created-event";
 
