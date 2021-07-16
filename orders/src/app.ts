52cc160afd41af770d5b955e420a46b4e220a3c7
Katrina <lichenkatrinani@gmail.com>
2021-07-15 21:21:20 -0700

    17. scaffolding the orders service

---
+++ orders/src/app.ts
@@ -0,0 +1,30 @@
+import "express-async-errors";
+
+import cookieSession from "cookie-session";
+import { errorHandler, currentUser } from "@kanitickets/common";
+import express from "express";
+import { json } from "body-parser";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
+import { indexOrderRouter } from "./routes";
+import { deleteOrderRouter } from "./routes/delete";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test", // https connection
+  })
+);
+
+app.use(currentUser);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
+
+app.use(errorHandler);
+
+export { app };
